9ab061b9bae6dbe1df3da866ad3bcef8d2bb961a
Sergey Aleinikov <chaositomail@gmail.com>
2020-02-21 09:34:26 +0600

    Array replaced to []

--- work-1/task5.php
+++ work-1/task5.php
@@ -1,12 +1,12 @@
 <?php
-    $bmw = array('model' => 'X5', 'speed' => 120, 'doors' => 5, 'year' => 2015);
-    $toyota = array('model' => 'supra', 'speed' => 110, 'doors' => 3, 'year' => 2000);
-    $opel = array('model' => 'speedster', 'speed' => 200, 'doors' => 3, 'year' => 2001);
-    
-    $cars = array('BMW' => $bmw, 'Toyota' => $toyota, 'Opel' => $opel);
-    
+    $bmw = ['model' => 'X5', 'speed' => 120, 'doors' => 5, 'year' => 2015];
+    $toyota = ['model' => 'supra', 'speed' => 110, 'doors' => 3, 'year' => 2000];
+    $opel = ['model' => 'speedster', 'speed' => 200, 'doors' => 3, 'year' => 2001];
+
+    $cars = ['BMW' => $bmw, 'Toyota' => $toyota, 'Opel' => $opel];
+
     print "CAR name<br>model speed doors year<br><br>";
-    
+
     foreach($cars AS $k => $v){
         print "CAR {$k}<br>";
         print implode(" ", $v)."<br><br>";
