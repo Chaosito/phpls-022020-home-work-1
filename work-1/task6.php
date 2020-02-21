b81946f3dde8269f8eb3ef120e2139b950da38e4
Sergey Aleinikov <chaositomail@gmail.com>
2020-02-21 10:07:07 +0600

    1. Isolated from html-tags 2. for(;;){ ... } changed to for(;;): ... endfor;

--- work-1/task6.php
+++ work-1/task6.php
@@ -1,31 +1,20 @@
-<?php
-    print '<table cellpadding="5" cellspacing="0" border="1">';
-    for ($i = 1; $i <= 10; $i++) {
-
-        if ($i == 1) {
-            print "<tr><td>&nbsp;</td>";
-            for ($j = 1; $j <= 10; $j++) {
-                print "<th>{$j}</th>";
-            }
-            print "</tr>";
-
-        }
-        // test23
-        print '<tr>';
-        for ($j = 1; $j <= 10; $j++) {
-
-            if ($j == 1) print "<th>{$i}</th>";
-
-            if ($i % 2 == 0 && $j % 2 == 0) {
-                $val = "(".($i * $j).")";
-            } elseif ($i % 2 != 0 && $j % 2 != 0){
-                $val = "[".($i * $j)."]";
-            } else {
-                $val = $i * $j;
-            }
-            print "<td style='text-align:center'>{$val}</td>";
-            // Если индекс строки и столбца четный, то в круглых, если не четный в квадратных, иначе тупо вывести
-        }
-        print '</tr>';
-    }
-    print '</table>';
+<table cellpadding="5" cellspacing="0" border="1">
+    <?php for ($i = 1; $i <= 10; $i++): ?>
+        <!--<tr><td><?=$i;?></td></tr>-->
+        <?php if ($i == 1) { ?>
+            <tr><td>&nbsp;</td>
+                <?php for ($j = 1; $j <= 10; $j++): ?>
+                    <th><?=$j;?></th>
+                <?php endfor; ?>
+            </tr>
+        <?php } ?>
+    <tr>
+        <?php for ($j = 1; $j <= 10; $j++): ?>
+            <?php if ($j == 1) { ?>
+                <th><?=$i;?></th>
+            <?php } ?>
+            <td style='text-align:center'><?=($i % 2 == 0 && $j % 2 == 0) ? "(".($i * $j).")" : (($i % 2 != 0 && $j % 2 != 0) ? "[".($i * $j)."]" : $i * $j);?></td>
+        <?php endfor; ?>
+    </tr>
+    <?php endfor; ?>
+</table>
